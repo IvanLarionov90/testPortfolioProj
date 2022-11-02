51b4a003f6e4557d53763db17edaf06900827393
IvanLarionov90 <gnosticism100500@gmail.com>
2022-11-02 12:59:51 +0600

    add new sections

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,10 @@ menuClose.addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('active');
 });
 
+const expItems = document.querySelectorAll('.scale__fill'),
+      percents = document.querySelectorAll('.percent');
+
+expItems.forEach((el, i) => {
+    el.style.width = percents[i].textContent;
+});
+
